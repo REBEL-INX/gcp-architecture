e5c80a821710c74c27dd4f689e8a56719bcdfff3
REBEL_INX <xdmiles@icloud.com>
2025-05-04 12:19:57 -0700

    update vpc

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -1,5 +1,23 @@
-resource "google_compute_network" "second" {
-  name                            = "second"
+resource "google_compute_network" "prod-vpc" {
+  name                            = "prod-vpc"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+
+resource "google_compute_network" "test-vpc" {
+  name                            = "test-vpc"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+
+resource "google_compute_network" "dev-vpc" {
+  name                            = "dev-vpc"
   routing_mode                    = "REGIONAL"
   auto_create_subnetworks         = false
   mtu                             = 1460
